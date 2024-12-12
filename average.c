15f3b0714e6c29064551c1d9eb039cb366f4c022
Shahd Elnasssag <shahdelnassag@gmail.com>
2024-12-12 15:10:30 +0200

    modifiy code in c language

--- average.c
+++ average.c
@@ -4,21 +4,24 @@
 float avarage(){
 
     printf("Shahd Elnassag ^_^\n");
-    // use it when want take numbers from user
-    int Array [10];
-    int counter = 0;
-    int sum = 0; 
-    while (counter < 10)
+    int cnt = 0;
+    int Array [10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int sum = 0;
+    int Array2 [10] = {7, 2, 5, 11, 4, 6, 1, 1, 8, 3};
+    int sum2 = 0;
+    while (cnt < 10)
     {
-        // use it when want take numbers from user
-        printf("Enter number %d of array: " , counter + 1);
-        scanf("%d" ,&Array[counter]);
-        // use it when Array is static
-        sum += Array[counter];
-        counter++;
+        sum += Array[cnt];
+        sum2 += Array2[cnt];
+        cnt++;
     }
+    // print test case 1
     float average = (float) sum / 10;
-    printf("Average is: %.1f\n" , average); 
+    printf("Average is: %.1f\n" , average);
+    // print test case 2
+    float average2 = (float) sum2 / 10;
+    printf("Average is: %.1f\n" , average2);
+
 }
 
 void main(){
