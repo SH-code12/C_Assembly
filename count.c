fa2296c7fcfd88d1c2fe5e1ec115a53f602a40ac
Shahd Elnasssag <shahdelnassag@gmail.com>
2024-12-12 17:19:09 +0200

    modifiy code count in c language

--- count.c
+++ count.c
@@ -1,27 +1,31 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int count(int arr[],int size){
-int count=0;
-    for( int i=0;i<size;++i){
-        if(arr[i]%2==0)count++;
+int count(){
+
+    printf("Asmaa Atef ^_^\n");
+    int cnt = 0;
+    int Array [10] = {10, 31, 5, 7, 11, 3, 8, 40, 12, 4};
+    int count1 = 0;
+    int Array2 [10] = {19, 2, 3, 7, 5, 10, 9, 0, 6, 1};
+    int count2 = 0;
+    while (cnt < 10)
+    {
+        if (Array[cnt] % 2 == 0)
+        {
+            count1++;
+        }
+        if (Array2[cnt] % 2 == 0)
+        {
+            count2++;
+        }
+        cnt++;
     }
-    return count;
+    // print test case 1
+    printf("Count of even numbers is: %d\n" , count1);
+    // print test case 2
+    printf("Count of even numbers is: %d\n" , count2);
 }
+void main(){
 
-
-int main() {
-    int arr1[] = {10, 31, 5, 7, 11, 3, 8, 40, 12, 4};
-    int arr2[] = {19, 2, 3, 7, 5, 10, 9, 0, 6, 1};
-    int size = sizeof(arr1) / sizeof(arr1[0]);
-
-
-    int count1 = count(arr1,size);
-    int count2 = count(arr2,size);
- 
-    printf("Count of even numbers for test1 is: %d\n", count1);
-
-
-    printf("Count of even numbers for test2 is: %d\n", count2);
-
-    return 0;
+    count();
 }
